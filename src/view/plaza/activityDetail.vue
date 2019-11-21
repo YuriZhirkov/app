d77f4680f6089f8bc531f42ac86ff09cd06fa782
YuriZhirkov <1562033492@qq.com>
2019-11-22 00:10:36 +0800

    微信分享

--- src/view/plaza/activityDetail.vue
+++ src/view/plaza/activityDetail.vue
@@ -128,7 +128,7 @@ export default {
       　var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
       　var maxPos = $chars.length;
       　var pwd = '';
-      　for (i = 0; i < len; i++) {
+      　for (let i = 0; i < len; i++) {
       　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
       　}
       　return pwd;
